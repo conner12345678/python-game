cc6f555ecf9ad0f229f8a6f81219db9ea8752b21
conner12345678 <cehly065@west-mec.org>
2023-12-30 10:37:08 -0700

    movement update
    remade the code for the movement and also made it so that you can move diagonally

--- python game/main.py
+++ python game/main.py
@@ -1,28 +1,69 @@
-import pygame
+import pygame, sys
 
+pygame.init()
 pygame.init()
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
+TITLE = "Test game"
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-player = pygame.Rect((300, 250, 50, 50))
-run = True
-while run:
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 0, 0), player)
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.move_ip(-1, 0)
-    elif key[pygame.K_d] == True:
-        player.move_ip(1, 0)
-    elif key[pygame.K_w] == True:
-        player.move_ip(0, -1)
-    elif key[pygame.K_s] == True:
-        player.move_ip(0, 1)
-    elif key[pygame.K_w] == True and key[pygame.K_d]:
-        player.move(1, 1)
+pygame.display.set_caption(TITLE)
+clock = pygame.time.Clock()
+class Player:
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+        self.rect = pygame.Rect(self.x, self.y, 32, 32)
+        self.color = (250, 120, 60)
+        self.velX = 0
+        self.velY = 0
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+        self.speed = 4
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+    def update(self):
+        self.velX = 0
+        self.velY = 0
+        if self.left_pressed and not self.right_pressed:
+            self.velX = -self.speed
+        if self.right_pressed and not self.left_pressed:
+            self.velX = self.speed
+        if self.up_pressed and not self.down_pressed:
+            self.velY = -self.speed
+        if self.down_pressed and not self.up_pressed:
+            self.velY = self.speed
+        self.x += self.velX
+        self.y += self.velY
+        self.rect = pygame.Rect(int(self.x), int(self.y), 32, 32)
+player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-    pygame.display.update()
-pygame.quit
+            pygame.quit
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.left_pressed = True
+            if event.key == pygame.K_RIGHT:
+                player.right_pressed = True
+            if event.key == pygame.K_UP:
+                player.up_pressed = True
+            if event.key == pygame.K_DOWN:
+                player.down_pressed = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.left_pressed = False
+            if event.key == pygame.K_RIGHT:
+                player.right_pressed = False
+            if event.key == pygame.K_UP:
+                player.up_pressed = False
+            if event.key == pygame.K_DOWN:
+                player.down_pressed = False
+    screen.fill((12, 24, 36))
+    player.draw(screen)
+    player.update()
+    pygame.display.flip()
+    clock.tick(120)
