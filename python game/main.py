cc689df9fdfda199d4c7d37eea59db90d6d388c1
conner12345678 <cehly065@west-mec.org>
2024-01-09 21:35:40 -0700

    redo
    Complete change to main  movement, and buggy collision

--- python game/main.py
+++ python game/main.py
@@ -1,69 +1,60 @@
-import pygame, sys
-
+import pygame
 pygame.init()
-pygame.init()
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 600
-TITLE = "Test game"
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption(TITLE)
+win = pygame.display.set_mode((500, 480))
+pygame.display.set_caption('The Game...')
 clock = pygame.time.Clock()
+run = True
 class Player:
-    def __init__(self, x, y):
-        self.x = int(x)
-        self.y = int(y)
-        self.rect = pygame.Rect(self.x, self.y, 32, 32)
-        self.color = (250, 120, 60)
-        self.velX = 0
-        self.velY = 0
-        self.left_pressed = False
-        self.right_pressed = False
-        self.up_pressed = False
-        self.down_pressed = False
-        self.speed = 4
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
     def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
-    def update(self):
-        self.velX = 0
-        self.velY = 0
-        if self.left_pressed and not self.right_pressed:
-            self.velX = -self.speed
-        if self.right_pressed and not self.left_pressed:
-            self.velX = self.speed
-        if self.up_pressed and not self.down_pressed:
-            self.velY = -self.speed
-        if self.down_pressed and not self.up_pressed:
-            self.velY = self.speed
-        self.x += self.velX
-        self.y += self.velY
-        self.rect = pygame.Rect(int(self.x), int(self.y), 32, 32)
-player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-while True:
+        pygame.draw.rect(win, (0, 255, 0), (self.x, self.y, self.width, self.height))
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
+class block:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
+    def draw(self, win):
+         pygame.draw.rect(win, (255, 0, 0), (self.hitbox))
+         self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
+def drawOnScreen():
+    man.draw(win)
+    wall.draw(win)
+    pygame.display.update()
+    win.fill((0, 0, 0))
+man = Player(30, 30, 60, 60) 
+wall = block(100, 100, 60, 60) 
+while run:
+    clock.tick(27)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player.left_pressed = True
-            if event.key == pygame.K_RIGHT:
-                player.right_pressed = True
-            if event.key == pygame.K_UP:
-                player.up_pressed = True
-            if event.key == pygame.K_DOWN:
-                player.down_pressed = True
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                player.left_pressed = False
-            if event.key == pygame.K_RIGHT:
-                player.right_pressed = False
-            if event.key == pygame.K_UP:
-                player.up_pressed = False
-            if event.key == pygame.K_DOWN:
-                player.down_pressed = False
-    screen.fill((12, 24, 36))
-    player.draw(screen)
-    player.update()
-    pygame.display.flip()
-    clock.tick(120)
+            run = False
+    keys = pygame.key.get_pressed()
+    if man.hitbox.colliderect(wall.hitbox):
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            man.y = man.y + 9
+        elif keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            man.x = man.x + 9
+        elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            man.y = man.y  - 9
+        elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            man.x = man.x - 9
+    else:
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            man.y -= man.vel
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            man.x -= man.vel
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            man.y += man.vel
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            man.x += man.vel
+    drawOnScreen()
+pygame.quit
